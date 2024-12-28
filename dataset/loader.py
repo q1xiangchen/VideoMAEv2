f29dfa4ecef77520bb4ac14b325737b500141773
Qixiang Chen <leseche88@gmail.com>
2024-12-29 03:21:31 +1100

    update loader

--- dataset/loader.py
+++ dataset/loader.py
@@ -121,35 +121,88 @@ def get_video_loader(use_petrel_backend: bool = True,
 def get_image_loader(use_petrel_backend: bool = True,
                      enable_mc: bool = True,
                      conf_path: str = None,
-                     data_root: str = "data/sthsthv2"):
+                     data_root: str = ""):
     if petrel_backend_imported and use_petrel_backend:
         _client = Client(conf_path=conf_path, enable_mc=enable_mc)
     else:
         _client = None
 
-    def _loader(frame_path):
-        # if _client is not None and 's3:' in frame_path:
-        #     img_bytes = _client.get(frame_path)
-        # else:
-        #     with open(frame_path, 'rb') as f:
-        #         img_bytes = f.read()
+    if data_root:
+        def _loader(frame_path):
+            # if _client is not None and 's3:' in frame_path:
+            #     img_bytes = _client.get(frame_path)
+            # else:
+            #     with open(frame_path, 'rb') as f:
+            #         img_bytes = f.read()
 
-        #NOTE: read image from h5 file
-        # find the only h5 file in the directory
-        dataset_path = None
-        for file in os.listdir(data_root):
-            if file.endswith(".h5"):
-                dataset_path = os.path.join(data_root, file)
-                break
-        if dataset_path is None:
-            raise ValueError("No h5 file found in the directory")
+            #NOTE: read image from h5 file
+            # find the only h5 file in the directory
+            dataset_path = None
+            for file in os.listdir(data_root):
+                if file.endswith(".h5"):
+                    dataset_path = os.path.join(data_root, file)
+                    break
+            if dataset_path is None:
+                with open(frame_path, 'rb') as f:
+                    img_bytes = f.read()
+            elif ".h5" in dataset_path:
+                video_binary = load_h5_file(dataset_path, frame_path)
+                img_bytes = io.BytesIO(video_binary)
+
+            img_np = np.frombuffer(img_bytes, np.uint8)
+            img = cv2.imdecode(img_np, cv2.IMREAD_COLOR)
+            cv2.cvtColor(img, cv2.COLOR_BGR2RGB, img)
+            return img
+
+        return _loader
+    else:
+        def _loader(frame_path):
+            if _client is not None and 's3:' in frame_path:
+                img_bytes = _client.get(frame_path)
+            else:
+                with open(frame_path, 'rb') as f:
+                    img_bytes = f.read()
+
+            img_np = np.frombuffer(img_bytes, np.uint8)
+            img = cv2.imdecode(img_np, cv2.IMREAD_COLOR)
+            cv2.cvtColor(img, cv2.COLOR_BGR2RGB, img)
+            return img
+
+        return _loader
+
+
+# def get_image_loader(use_petrel_backend: bool = True,
+#                      enable_mc: bool = True,
+#                      conf_path: str = None,
+#                      data_root: str = "data/sthsthv2"):
+#     if petrel_backend_imported and use_petrel_backend:
+#         _client = Client(conf_path=conf_path, enable_mc=enable_mc)
+#     else:
+#         _client = None
+
+#     def _loader(frame_path):
+#         # if _client is not None and 's3:' in frame_path:
+#         #     img_bytes = _client.get(frame_path)
+#         # else:
+#         #     with open(frame_path, 'rb') as f:
+#         #         img_bytes = f.read()
+
+#         #NOTE: read image from h5 file
+#         # find the only h5 file in the directory
+#         dataset_path = None
+#         for file in os.listdir(data_root):
+#             if file.endswith(".h5"):
+#                 dataset_path = os.path.join(data_root, file)
+#                 break
+#         if dataset_path is None:
+#             raise ValueError("No h5 file found in the directory")
         
-        video_binary = load_h5_file(dataset_path, frame_path)
-        img_bytes = io.BytesIO(video_binary)
+#         video_binary = load_h5_file(dataset_path, frame_path)
+#         img_bytes = io.BytesIO(video_binary)
 
-        img_np = np.frombuffer(img_bytes, np.uint8)
-        img = cv2.imdecode(img_np, cv2.IMREAD_COLOR)
-        cv2.cvtColor(img, cv2.COLOR_BGR2RGB, img)
-        return img
+#         img_np = np.frombuffer(img_bytes, np.uint8)
+#         img = cv2.imdecode(img_np, cv2.IMREAD_COLOR)
+#         cv2.cvtColor(img, cv2.COLOR_BGR2RGB, img)
+#         return img
 
-    return _loader
+#     return _loader
