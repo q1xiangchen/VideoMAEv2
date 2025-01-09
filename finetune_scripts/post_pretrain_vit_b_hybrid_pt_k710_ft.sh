7d22bc6b46507dff5d49cf38b85bef73678d158e
Qixiang Chen <leseche88@gmail.com>
2025-01-10 02:01:16 +1100

    update the code and scripts

---
+++ finetune_scripts/post_pretrain_vit_b_hybrid_pt_k710_ft.sh
@@ -0,0 +1,48 @@
+
+#!/usr/bin/env bash
+set -x
+
+export MASTER_PORT=$((12000 + $RANDOM % 20000))
+export OMP_NUM_THREADS=1
+
+OUTPUT_DIR='./finetune_results/k710/post_pretrain'
+DATA_PATH='PATH_TO_K710_DATASET'
+MODEL_PATH='./model_zoo/PATH_TO_PRETRAINED_MODEL'
+
+N_NODES=1
+GPUS_PER_NODE=4
+SRUN_ARGS=${SRUN_ARGS:-""}
+PY_ARGS=${@:2}
+
+# batch_size can be adjusted according to the graphics card
+torchrun --nproc_per_node=${GPUS_PER_NODE} \
+        --master_port ${MASTER_PORT} --nnodes=${N_NODES} \
+        run_class_finetuning.py \
+        --model vit_base_patch16_224 \
+        --data_set Kinetics-710 \
+        --nb_classes 710 \
+        --data_path ${DATA_PATH} \
+        --finetune ${MODEL_PATH} \
+        --log_dir ${OUTPUT_DIR} \
+        --output_dir ${OUTPUT_DIR} \
+        --batch_size 16 \
+        --input_size 224 \
+        --short_side_size 224 \
+        --save_ckpt_freq 10 \
+        --num_frames 16 \
+        --sampling_rate 4 \
+        --num_sample 2 \
+        --num_workers 10 \
+        --opt adamw \
+        --lr 7e-4 \
+        --drop_path 0.1 \
+        --head_drop_rate 0.0 \
+        --layer_decay 0.75 \
+        --opt_betas 0.9 0.999 \
+        --warmup_epochs 5 \
+        --epochs 90 \
+        --test_num_segment 5 \
+        --test_num_crop 3 \
+        --motion_layer baseline \
+        --dist_eval  \
+        --end_to_end \
